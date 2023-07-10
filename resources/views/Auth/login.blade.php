5676ab4d5a1c02df0765490e48c17cc735223394
shad kamil hama saeed <54715323+shadKamel@users.noreply.github.com>
2023-07-10 09:45:50 +0300

    slaw

---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.guest')
+
+@section('content')
+    <form action="">
+        <div>
+            <x-input />
+        </div>
+    </form>
+@endsection
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,1 @@
+<input type="text" class="px-3 py-2 rounded-md ring-1 ring-gray-300" />
