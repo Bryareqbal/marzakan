45fc4e549dff07ebb529cfa1767b62088629e4ce
Hoshmand Kamal <hoshmand.G900@gmail.com>
2023-07-13 20:24:58 +0300

    the report is finished

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,13 +13,11 @@ class ReportController extends Controller
 {
     public function index(Request $request)
     {
-        $sumPrice = 0;
-        $count_of_sardanikat = 0;
         $karmandkan = Karmand::all();
         $sarprshtyarkan = Sarparshtyar::all();
         $marzakan = Marzakan::all();
 
-        $reports = sardanikar::with(['karmand'])->when(!empty($request->search), function (Builder $query) use ($request) {
+        $query = sardanikar::with(['karmand'])->when(!empty($request->search), function (Builder $query) use ($request) {
             $query->where(function (Builder $query) use ($request) {
                 $query->orWhere('name', 'like', "%{$request->search}%")
                        ->orWhere('password_number', 'like', "%{$request->search}%")
@@ -40,28 +38,21 @@ class ReportController extends Controller
             $query->whereHas('karmand', function (Builder $query) use ($request) {
                 $query->whereHas('sarparshtyar', function (Builder $query) use ($request) {
                     $query->where('marz_id', 'like', "%{$request->marz_id}%");
-
                 });
             });
 
         })->when(!empty($request->created_at), function (Builder $query) use ($request) {
             $query->where('created_at', 'like', "%{$request->created_at}%");
-        })
+        });
 
-        ->latest()->paginate(25);
         $request->flash('*');
 
-        foreach($reports as $key => $report) {
-            $sumPrice += $report->mount_of_money;
-        }
-        $count_of_sardanikat += $reports->count('id');
         return view('Admin.Reports.Report', [
-                'reports' => $reports,
+                'reports' => $query->latest()->paginate(25),
                 'karmandkan'=>$karmandkan,
                 'sarprshtyarkan'=>$sarprshtyarkan,
                 'marzakan'=>$marzakan,
-                'sumPrice'=>$sumPrice,
-                'count_of_sardanikat'=>$count_of_sardanikat
+                'sumPrice'=>$query->sum('mount_of_money'),
         ]);
     }
 
--- resources/views/Admin/Reports/Report.blade.php
+++ resources/views/Admin/Reports/Report.blade.php
@@ -124,8 +124,8 @@
             <div class="border-opacity-15 w-1/3 rounded-xl border p-5 shadow-xl hover:drop-shadow-xl">
                 <ul class="flex items-center justify-between">
                     <li>کۆی سەردانیکەر</li>
-                    @if ($count_of_sardanikat)
-                        <li class="font-medium">{{ number_format($count_of_sardanikat, 0, '.', ',') }}</li>
+                    @if ($reports->total() !== 0)
+                        <li class="font-medium">{{ number_format($reports->total(), 0, '.', ',') }}</li>
                     @else
                         <li class="font-medium">{{ number_format(0, 0, '.', ',') }}</li>
                     @endif
@@ -148,7 +148,9 @@
                             <th class="py-3 px-1 text-center font-medium">بڕی پارە</th>
                             <th class="py-3 px-1 text-center font-medium">حاڵات</th>
                             <th class="py-3 px-1 text-center font-medium">بەرواری زیادکردن</th>
-                            <th class="py-3 px-1 text-center font-medium">چالاکی</th>
+                            @canany(['superadmin', 'sarparshtyar'])
+                                <th class="py-3 px-1 text-center font-medium">چالاکی</th>
+                            @endcanany
                         </tr>
                     </thead>
                     <tbody>
@@ -167,6 +169,7 @@
                                 <td class="py-3 px-6 text-center">{{ $report->nation }}</td>
                                 <td class="py-3 px-6 text-center">{{ $report->phone }}</td>
                                 <td class="py-3 px-6 text-center">
+
                                     {{ number_format($report->mount_of_money, 0, '.', ',') }}
                                 </td>
                                 <td class="py-3 px-6 text-center">
@@ -193,18 +196,20 @@
                                 </td>
                                 <td class="py-3 px-6 text-center font-sans">
                                     {{ $report->created_at->format('Y-m-d H:i:s A') }}</td>
-                                <td class="py-3 px-6 text-center font-sans">
-                                    <a title="گۆرانکاری"
-                                        class="flex h-10 w-10 items-center justify-center rounded border-2 border-blue-200 text-xl hover:border-blue-300 focus:ring-1 focus:ring-blue-500 focus:ring-offset-2">
+                                @canany(['superadmin', 'sarparshtyar'])
+                                    <td class="py-3 px-6 text-center font-sans">
+                                        <a href="{{ route('edit-sardanikar', $report->id) }}" title="گۆرانکاری"
+                                            class="flex h-10 w-10 items-center justify-center rounded border-2 border-blue-200 text-xl hover:border-blue-300 focus:ring-1 focus:ring-blue-500 focus:ring-offset-2">
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-blue-500">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                        </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-blue-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
 
-                                    </a>
-                                </td>
+                                        </a>
+                                    </td>
+                                @endcanany
                             </tr>
                         @endforeach
                     </tbody>
